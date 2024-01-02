8510b3123db7eaac252c8f6b1ff7f55338424be7
Ekaterina Goldinova <shadowkatja@gmail.com>
2024-01-02 22:18:21 +0300

    init commit

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+from selene.support.shared import browser
+
+
+@pytest.fixture(scope='function', autouse=True)
+def set_browser():
+    browser.config.base_url = 'https://automationexercise.com'
+    browser.config.timeout = 10
+    browser.config.window_width = 1280
+    browser.config.window_height = 1024
+
+    yield
+
+    browser.quit()

2ffd58f63406291b4dbc3fe59a4a85c7f54affb5
Ekaterina Goldinova <shadowkatja@gmail.com>
2024-01-10 11:04:51 +0300

    change timeout

--- tests/UI/conftest.py
+++ tests/UI/conftest.py
@@ -51,7 +51,7 @@ def set_browser(request):
     browser.config.driver = driver
 
     browser.config.base_url = 'https://automationexercise.com/'
-    browser.config.timeout = 10
+    browser.config.timeout = 15
     browser.config.window_width = 1280
     browser.config.window_height = 1024
 
